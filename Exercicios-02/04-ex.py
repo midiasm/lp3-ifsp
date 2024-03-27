e01f41c80b666ddbdb345b3fc283048b6161f706
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-03-27 13:57:00 -0300

    correção

--- Exercicios-02/04-ex.py
+++ Exercicios-02/04-ex.py
@@ -1,2 +1,50 @@
 #Ex04 - Simulador de Eleições: Crie um programa que simule uma votação com três candidatos. O programa deve pedir ao usuário para votar várias vezes e, no final, mostrar o número de votos de cada candidato e quem foi o vencedor. 
 
+candidatos = [
+    {
+        "nome": "Candidato 1",
+        "numero": "13",
+        "votos": 0,
+    },
+    {
+        "nome": "Candidato 3",
+        "numero": "14",
+        "votos": 0,
+    },
+    {
+        "nome": "Candidato 2",
+        "numero": "15",
+        "votos": 0,
+    },
+]
+
+print("ELEIÇÕES 2024")
+print("Candidatos: ")
+
+for candidato in candidatos:
+    print(f"{candidato["nome"]} : {candidato["numero"]}")
+
+for i in range(2):
+    voto = input("Vote em um candidato: ")
+
+    for candidato in candidatos:
+        if voto == candidato["numero"]:
+            candidato["votos"] += 1
+
+vencedor = {
+    "nome": "",
+    "numero": "0",
+    "votos": 0
+}
+
+print("Resultados da votação: ")
+
+for candidato in candidatos:
+    print(f"{candidato["nome"]}: {candidato["votos"]}")
+
+    if candidato["votos"] > vencedor["votos"]:
+        vencedor = candidato
+
+print(f"{vencedor["nome"]} foi eleito!")
+print(f"Número eleitoral: {vencedor["numero"]}")
+print(f"Total de votos: {vencedor["votos"]}")
--- Exercicios-02/08-ex.py
+++ Exercicios-02/08-ex.py
@@ -1,1 +1,21 @@
 #Ex08 - Função de Contagem de Palavras: Escreva uma função que receba uma string (frase) como argumento e retorne um dicionário onde as chaves são as palavras únicas no texto e os valores são o número de vezes que cada palavra aparece no texto. Depois, teste a função com diferentes textos fornecidos pelo usuário.
+
+def contar_palavras(frase: str):
+    contagem_palavras = {}
+    
+    for invalido in (".", ",", "/", "?", "!", "'", "0", "1", "2", "3", "4", "6", "7", "8", "9"):
+        frase = frase.replace(invalido, "")
+
+    frase = frase.split(" ")
+
+    for palavra in frase:
+        palavra = palavra.replace(",", "")
+        palavra = palavra.replace(".", "")
+
+        contagem_palavras[palavra] = 0
+        contagem_palavras[palavra] = frase.count(palavra)
+
+    return contagem_palavras
+
+frase = input("Digite uma frase para contar as palavras: ")
+print(contar_palavras(frase))
